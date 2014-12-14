78bb8595da315d8266f888046a941b4b81cec2e9
Alessandro Siragusa <alessandro.siragusa@gmail.com>
2014-12-14 13:03:05 +0000

    Added option to use etcd

--- nerve/run.sh
+++ nerve/run.sh
@@ -3,9 +3,32 @@
 function join { local IFS="$1"; shift; echo "$*"; }
 function split { echo $1 | cut -d : -f $2; }
 
+function error {
+    echo "$1" >&2
+    help
+}
+
 function help {
     echo "TBD"
     exit 1
+}
+
+function check_env_vars {
+    if [ -z "$SERVICE" ]; then error "You must specify a service path"; fi
+    if [ -z "$SERVICE_HOST" ]; then error "You must specify a service host"; fi
+    if [ -z "$SERVICE_PORT" ]; then error "You must specify a service port"; fi
+
+    INSTANCE_ID=${INSTANCE_ID:-nerve}
+    CHECK_INTERVAL=${CHECK_INTERVAL:-2}
+
+    C_TIMEOUT=${C_TIMEOUT:-0.2}
+    C_RISE=${C_RISE:-3}
+    C_FAIL=${C_FAIL:-2}
+
+    ZK_PATH=${ZK_PATH:-/nerve/services/}
+
+    ETCD_PATH=${ETCD_PATH:-/nerve/services/}
+    ETCD_PORT=${ETCD_PORT:-4001}
 }
 
 function set_name_idx {
@@ -18,7 +41,9 @@ function set_name_idx {
     fi
 }
 
-function set_servers {
+function set_zk_hosts {
+    if [ -z "$ZK_HOSTS" ]; then error "No Zookeeper hosts specified"; fi
+
     local IFS=","
     local IDX=0
     read -ra SERVERS <<< "$ZK_HOSTS"
@@ -30,18 +55,37 @@ function set_servers {
 }
 
 function init {
+    check_env_vars
+
     FILTERS+=(".instance_id=\"$INSTANCE_ID\"")
     FILTERS+=(".services={}")
     FILTERS+=(".services.default={}")
 
     FILTERS+=(".services.default.host=\"$SERVICE_HOST\"")
-    FILTERS+=(".services.default.port=$PORT")
-    FILTERS+=(".services.default.reporter_type=\"zookeeper\"")
-    FILTERS+=(".services.default.zk_path=\"/nerve/services/$ZK_PATH\"")
+    FILTERS+=(".services.default.port=$SERVICE_PORT")
     FILTERS+=(".services.default.check_interval=$CHECK_INTERVAL")
 
-    FILTERS+=(".services.default.zk_hosts=[]")
     FILTERS+=(".services.default.checks=[]")
+}
+
+function set_zookeeper {
+    FILTERS+=(".services.default.reporter_type=\"zookeeper\"")
+    FILTERS+=(".services.default.zk_path=\"$ZK_PATH$SERVICE\"")
+    FILTERS+=(".services.default.zk_hosts=[]")
+
+    set_zk_hosts
+    REPORTER=true
+}
+
+function set_etcd {
+    if [ -z "$ETCD_HOST" ]; then error "You must specify an etcd host"; fi
+
+    FILTERS+=(".services.default.reporter_type=\"etcd\"")
+    FILTERS+=(".services.default.etcd_path=\"$ETCD_PATH$SERVICE\"")
+    FILTERS+=(".services.default.etcd_host=\"$ETCD_HOST\"")
+    FILTERS+=(".services.default.etcd_port=$ETCD_PORT")
+
+    REPORTER=true
 }
 
 function set_filter {
@@ -56,45 +100,22 @@ function set_filter {
 }
 
 function set_standard_filter {
-      set_filter "$1:name:$1"
-      set_filter "$1:type:$1"
-      set_filter "$1:timeout:$C_TIMEOUT"
-      set_filter "$1:rise:$C_RISE"
-      set_filter "$1:fail:$C_FAIL"
+    set_filter "$1:name:$1"
+    set_filter "$1:type:$1"
+    set_filter "$1:timeout:$C_TIMEOUT"
+    set_filter "$1:rise:$C_RISE"
+    set_filter "$1:fail:$C_FAIL"
 }
-
-if [ -z "$CHECK_INTERVAL" ]
-then
-    CHECK_INTERVAL=2
-fi
-
-if [ -z "$C_TIMEOUT" ]
-then
-    C_TIMEOUT=0.2
-fi
-
-if [ -z "$C_RISE" ]
-then
-    C_RISE=3
-fi
-
-if [ -z "$C_FAIL" ]
-then
-    C_FAIL=2
-fi
 
 NAME_IDX=0
 declare -A NAMES;
 declare -a FILTERS;
 
 init
-set_servers
 
-OPTS=`getopt -o "c:o:" -l "http:,tcp,rabbitmq:" -- "$@"`
-if [ $? != 0 ]
-then
-    help
-fi
+OPTS=`getopt -o "c:o:" -l "http:,tcp,rabbitmq:,zk,etcd" -- "$@"`
+
+if [ $? != 0 ]; then error "Error parsing arguments"; fi
 
 eval set -- "$OPTS"
 
@@ -109,6 +130,12 @@ while true ; do
     -o)
       set_filter $2
       shift 2;;
+    --zk)
+      set_zookeeper
+      shift;;
+    --etcd)
+      set_etcd
+      shift;;
     --tcp)
       set_standard_filter "tcp"
       shift;;
@@ -126,6 +153,8 @@ while true ; do
     --) shift; break;;
   esac
 done
+
+if [ -z "$REPORTER" ]; then error "You have to set at least one reporter"; fi
 
 JQ_FILTERS=$(join "|" "${FILTERS[@]}")
 
